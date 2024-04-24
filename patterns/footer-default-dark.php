7d5117ce01697ac5ad6cf3fec2bc2fb41303149e
unionglyph <167571353+unionglyph@users.noreply.github.com>
2024-04-24 13:31:10 -0400

    Update footer-default-dark.php

--- patterns/footer-default-dark.php
+++ patterns/footer-default-dark.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Title: Footer with text, links.
- * Slug: frost/footer-default-dark
+ * Slug: sycamore/footer-default-dark
  * Categories: footer
  * Block Types: core/template-part/footer
  */
@@ -9,10 +9,10 @@
 <!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|small","bottom":"var:preset|spacing|small","left":"30px","right":"30px"},"margin":{"top":"0px"}},"elements":{"link":{"color":{"text":"var:preset|color|background"}}}},"backgroundColor":"contrast","textColor":"base","className":"has-background-color","layout":{"type":"constrained"},"fontSize":"small"} -->
 <div class="wp-block-group alignfull has-background-color has-base-color has-contrast-background-color has-text-color has-background has-link-color has-small-font-size" style="margin-top:0px;padding-top:var(--wp--preset--spacing--small);padding-right:30px;padding-bottom:var(--wp--preset--spacing--small);padding-left:30px"><!-- wp:group {"align":"wide","layout":{"type":"flex","allowOrientation":false,"justifyContent":"space-between"}} -->
 <div class="wp-block-group alignwide"><!-- wp:paragraph -->
-<p>&copy; <?php echo esc_html( gmdate( 'Y' ) ); ?> <?php echo esc_html__( 'Your Company LLC', 'frost' ); ?> · <a href="#"><?php echo esc_html__( 'Contact Us', 'frost' ); ?></a></p>
+<p>&copy; <?php echo esc_html( gmdate( 'Y' ) ); ?> <?php echo esc_html__( 'Your Company LLC', 'sycamore' ); ?> · <a href="#"><?php echo esc_html__( 'Contact Us', 'sycamore' ); ?></a></p>
 <!-- /wp:paragraph -->
 <!-- wp:paragraph -->
-<p><a href="#"><?php echo esc_html__( 'Facebook', 'frost' ); ?></a> · <a href="#"><?php echo esc_html__( 'Twitter', 'frost' ); ?></a> · <a href="#"><?php echo esc_html__( 'Instagram', 'frost' ); ?></a></p>
+<p><a href="#"><?php echo esc_html__( 'Facebook', 'sycamore' ); ?></a> · <a href="#"><?php echo esc_html__( 'Twitter', 'sycamore' ); ?></a> · <a href="#"><?php echo esc_html__( 'Instagram', 'sycamore' ); ?></a></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
