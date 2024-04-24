b587cdc96d05ec72cd879d6a1684b11a7545e257
unionglyph <167571353+unionglyph@users.noreply.github.com>
2024-04-24 13:29:09 -0400

    Update footer-three-columns-dark.php

--- patterns/footer-three-columns-dark.php
+++ patterns/footer-three-columns-dark.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Title: Footer with text, links, buttons.
- * Slug: frost/footer-three-columns-dark
+ * Slug: sycamore/footer-three-columns-dark
  * Categories: footer
  * Block Types: core/template-part/footer
  */
@@ -10,10 +10,10 @@
 <div class="wp-block-group alignfull has-background-color has-base-color has-contrast-background-color has-text-color has-background has-link-color has-small-font-size" style="margin-top:0px;padding-top:var(--wp--preset--spacing--small);padding-right:30px;padding-bottom:var(--wp--preset--spacing--small);padding-left:30px"><!-- wp:group {"align":"wide","layout":{"type":"flex","allowOrientation":false,"justifyContent":"space-between"}} -->
 <div class="wp-block-group alignwide">
 <!-- wp:paragraph -->
-<p>&copy; <?php echo esc_html( gmdate( 'Y' ) ); ?> <?php echo esc_html__( 'Your Company LLC', 'frost' ); ?></p>
+<p>&copy; <?php echo esc_html( gmdate( 'Y' ) ); ?> <?php echo esc_html__( 'Your Company LLC', 'sycamore' ); ?></p>
 <!-- /wp:paragraph -->
 <!-- wp:paragraph -->
-<p><a href="#"><?php echo esc_html__( 'Privacy Policy', 'frost' ); ?></a> · <a href="#"><?php echo esc_html__( 'Terms of Service', 'frost' ); ?></a> · <a href="#"><?php echo esc_html__( 'Contact Us', 'frost' ); ?></a></p>
+<p><a href="#"><?php echo esc_html__( 'Privacy Policy', 'sycamore' ); ?></a> · <a href="#"><?php echo esc_html__( 'Terms of Service', 'sycamore' ); ?></a> · <a href="#"><?php echo esc_html__( 'Contact Us', 'sycamore' ); ?></a></p>
 <!-- /wp:paragraph -->
 <!-- wp:social-links {"iconColor":"contrast","iconColorValue":"#000","iconBackgroundColor":"base","iconBackgroundColorValue":"#fff","className":"is-style-default","style":{"spacing":{"blockGap":"10px"}}} -->
 <ul class="wp-block-social-links has-icon-color has-icon-background-color is-style-default">
