1f78d52d79f58e7fa26105632e4ce428ae9bbddb
unionglyph <167571353+unionglyph@users.noreply.github.com>
2024-04-24 13:28:10 -0400

    Update header-default.php

--- patterns/header-default.php
+++ patterns/header-default.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Title: Header with site title, navigation.
- * Slug: frost/header-default
+ * Slug: sycamore/header-default
  * Categories: header
  * Block Types: core/template-part/header
  */
